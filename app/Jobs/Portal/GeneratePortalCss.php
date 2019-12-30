83ae5a767cc0a27d272ef50c9013229c9ac1423e
Wim <wim@mosmania.nl>
2019-12-30 13:05:55 +0100

    Temp change removed: find location of path where portal.css is saved.

--- app/Jobs/Portal/GeneratePortalCss.php
+++ app/Jobs/Portal/GeneratePortalCss.php
@@ -40,7 +40,6 @@ class GeneratePortalCss implements ShouldQueue
         ])->render();
         try{
             Storage::disk('public_portal')->put('portal.css', $html);
-            Log::error('Opgeslagen in : ' . Storage::disk('public_portal')->path('portal.css'));
         }catch (Exception $exception){
             Log::error('Opslaan gewijzigde portal.css mislukt : ' . $exception->getMessage());
         }
