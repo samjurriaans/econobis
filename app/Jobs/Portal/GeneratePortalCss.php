4d9c9bd009f5b26edf0378b4fc2ede58cc72e0f4
Wim <wim@mosmania.nl>
2019-12-30 12:59:18 +0100

    Temp change: find location of path where portal.css is saved.

--- app/Jobs/Portal/GeneratePortalCss.php
+++ app/Jobs/Portal/GeneratePortalCss.php
@@ -8,6 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Spatie\Valuestore\Valuestore;
 
