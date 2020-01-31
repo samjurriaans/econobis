6b70cc84126fb56a83a37a7dbefd37f62b8055d6
Wim <wim@mosmania.nl>
2020-01-31 13:07:48 +0100

    npm build customer-portal-app

--- customer-portal-app/build/service-worker.js
+++ customer-portal-app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.fd4dc6c650cdd2cab9bf15305d669716.js"
+  "./precache-manifest.78f4b5cc7c3878e0f60cb1527ac9c9de.js"
 );
 
 self.addEventListener('message', (event) => {
